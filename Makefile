ba4244550c8fe61e4dd282ac6254178e5e638a7b
nelitow <ngvjqp@gmail.com>
2017-12-19 19:36:44 -0200

    cu de cachorro do caralho

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
-obj-m += hello.o
+obj-m += char_device.o
+
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- char_device.cpp
+++ char_device.cpp
@@ -1,23 +1,18 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
+/*
+ *  hello-1.c - The simplest kernel module.
+ */
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
 
 int init_module(void){
-	printk(KERN_INFO "Hello world\n");
+	printk(KERN_INFO "Hello world 1.\n");
+
+	/*
+	 * A non 0 return means init_module failed; module can't be loaded.
+	 */
 	return 0;
 }
 
 void cleanup_module(void){
-	printk(KERN_INFO "Bye world\n");
+	printk(KERN_INFO "Goodbye world 1.\n");
 }
-
-//codigos no quadro
-/dev/meudev
-int meuopen(struct inode* in, struct file* s){}
-
-file_operations opts;
-opts.open = meuopen;
-opts.write = meuwrite;
-int fd = open("/dev/ttyS0");
-write(fd, 'x');
-
-ssize_t meuwrite(struct file * t , const char * c){}
